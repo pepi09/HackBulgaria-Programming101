e5089691d0856c668404bdbb6510da82065bdfa6
pepi09 <pepa.p.simeonova@abv.bg>
2014-03-23 22:40:40 +0200

    Adding changes to sudoku_solved

---
+++ week0/sudoku_solved/solution.py
@@ -0,0 +1,34 @@
+def sudoku_solved(sudoku):
+	example = [1,2,3,4,5,6,7,8,9]
+	flag = True
+	for i in sudoku:
+		if sorted(i) != example:
+			return False
+	for i in range(9):
+		if sorted(get_column(sudoku,i)) != example:
+			return False
+	while i <= 6: 
+		if sorted(get_square(sudoku,i,0)) != example:
+			return False
+		if sorted(get_square(sudoku,i,3)) != example:
+			return False
+		if sorted(get_square(sudoku,i,6)) != example:
+			return False
+		i+=3
+	return True
+
+
+
+def get_column(matrix,index):
+    column = []
+    for i in range(0,len(matrix)):
+        column.append(matrix[i][index])
+    return column
+
+def get_square(matrix,row,column): #row,column - indexes of the first elemenr of the square
+	square = []
+	for i in range(3):
+		for j in range(3):
+			square.append(matrix[row + i][column + j])
+	print (sorted(square))
+	return square
